939c1ac53a3e79ddeba5203cde340cb51d0ed80d
unknown <you@example.com>
2021-01-15 14:01:35 +0700

    update

--- Module 3/bai_5_SOL_index_View_Stored_Procedure/bai_tap/demo.sql
+++ Module 3/bai_5_SOL_index_View_Stored_Procedure/bai_tap/demo.sql
@@ -17,6 +17,99 @@ insert into products values
 (4,'asfdf','Tho',500,2,'Son','Moi'),
 (5,'gfgfd','Vu',5000,1,'Giay','Cu');
 
+explain select *
+from products
+where product_code = 'asfds';
+
+create unique index idx_product on products (product_code);
+
+explain select product_code
+from products
+where product_code = 'asfds';
+
+create view product_view as
+select
+ product_code,
+ product_name,
+ product_price,
+ product_status
+ from products;
+ select * 
+ from product_view;
+ 
+ create or replace view product_view as
+ select 
+ product_code,
+ product_name,
+ product_price,
+ product_status
+ from products
+ where id = 1;
+ 
+ drop view product_view;
+ 
+ DELIMITER \\ 
+ create procedure find_all_product ()
+ begin
+ select *
+ from products;
+ end \\
+ DELIMITER ;
+
+call find_all_product();
+
+delimiter \\
+create  procedure add_new_product (
+	id int,
+    product_code varchar(50),
+    product_name varchar(50),
+    product_price decimal(9,2),
+    product_amount int,
+    product_description varchar(100),
+    product_status varchar(100)
+)
+begin 
+	insert into products 
+    values (id,product_code,product_name,product_price,product_amount,product_description,product_status);
+    end;
+\\ delimiter ;
+
+call add_new_product(6,'dgtrh','Ngoc',15000,2,'Vay','Moi');
+
+delimiter \\
+create procedure edit_product (p_keyword varchar(50), id int)
+begin
+update products
+set product_name = p_keyword
+where products.id = id;
+end;\\
+delimiter ;
+
+call edit_product ('a1', 3);
+drop procedure edit_product;
+
+delimiter \\
+create procedure delete_product (id int)
+begin
+delete from products
+where products.id = id;
+end;\\
+delimiter ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
